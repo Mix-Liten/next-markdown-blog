94c2b92a7481c587bec6617b9029d463323cc362
Mix-Liten <mixliten@gmail.com>
2021-08-22 01:23:20 +0800

    feat: add Post(card)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import Post from '@/components/Post'
+import { getPosts } from '@/lib/posts'
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
   return (
     <Layout>
-      <h2>content</h2>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
     </Layout>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: getPosts().slice(0, 6),
+    },
+  }
 }
